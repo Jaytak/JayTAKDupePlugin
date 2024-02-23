5602df1a25a47c441fb4934095b36c506a364180
Jay <78488517+Jaytak@users.noreply.github.com>
2024-02-23 19:35:01 +1300

    V 0.0.8 Fixed a bug that was deleting items... Added more information to user when running commends.

--- src/main/java/io/jaytak/jaytakdupeplugin/Dupe.java
+++ src/main/java/io/jaytak/jaytakdupeplugin/Dupe.java
@@ -7,6 +7,7 @@ import org.bukkit.inventory.ItemStack;
 import org.bukkit.Material;
 import org.checkerframework.checker.nullness.qual.NonNull;
 
+
 import java.util.Arrays;
 
 import static org.bukkit.Bukkit.getLogger;
@@ -39,6 +40,7 @@ public class Dupe implements CommandExecutor {
                                     else if (args.length == 2){
                                         String operator = args[0];
                                         int amt = Integer.parseInt(args[1]);
+
                                         switch (operator){
                                             case "+":
                                                 finalAmt = count + amt;
@@ -58,10 +60,7 @@ public class Dupe implements CommandExecutor {
                                                 sender.sendMessage("[JayTak Dupe] Invalid Operator: " + operator);
                                                 return false;
                                         }
-                                        if (finalAmt < 1){
-                                            finalAmt = 1;
-                                        }
-                                    }
+                                                                            }
                                     else{
                                         count++;
                                         finalAmt = count;
@@ -75,11 +74,27 @@ public class Dupe implements CommandExecutor {
                                     sender.sendMessage("[JayTAK Dupe] Ran into Exception.");
                                     getLogger().info("[JayTAK Dupe] Ran into Exception: " + e);
                                 }
-                                //count++;
+
+                                if (finalAmt < 1){
+                                    sender.sendMessage("[JayTAK Dupe] Hmmmm, seems sus.... Final Amount: " + finalAmt);
+                                    finalAmt = 1;
+                                    sender.sendMessage("[JayTAK Dupe] Lucky for you i thought about this.");
+                                    sender.sendMessage("[JayTAK Dupe] Final Amount Clipped To: " + finalAmt);
+                                }
+                                if (finalAmt > 64){
+                                    sender.sendMessage("JayTAK Dupe] Unsafe Final Amount: " + finalAmt + ". Capped to 64.");
+                                    getLogger().info("JayTAK Dupe] Unsafe Final Amount: " + finalAmt + ". Capped to 64.");
+                                    finalAmt = 64;
+                                }
+
                                 itemInHand.setAmount(finalAmt);
-                                getLogger().info("[JayTAK Dupe] Duped items. Final amt: " + finalAmt);
+                                getLogger().info("[JayTAK Dupe] Duped items. Final Amount: ");
                                 sender.sendMessage("[JayTAK Dupe] Duped items. Final amount: " + finalAmt);
                                 return true;
+                            }
+                            else{
+                                sender.sendMessage("[JayTAK Dupe] You need to be holding an item to dupe, cant dupe nothin!");
+                                getLogger().info("[JayTAK Dupp] Player" + username + " wasnt holding item.");
                             }
                         }catch (Exception exception){
                             getLogger().info("[JayTak Dupe] exception: " + exception);
