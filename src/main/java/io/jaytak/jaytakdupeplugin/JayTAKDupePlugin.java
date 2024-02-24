6ba6388a45fda0c80e4746f7c4a4fe84997463ce
Jay <78488517+Jaytak@users.noreply.github.com>
2024-02-24 16:56:04 +1300

    Added bStats.

--- src/main/java/io/jaytak/jaytakdupeplugin/JayTAKDupePlugin.java
+++ src/main/java/io/jaytak/jaytakdupeplugin/JayTAKDupePlugin.java
@@ -2,12 +2,17 @@ package io.jaytak.jaytakdupeplugin;
 import java.util.Objects;
 import org.bukkit.plugin.java.JavaPlugin;
 
+@SuppressWarnings({"unused"})
 public final class JayTAKDupePlugin extends JavaPlugin {
 
     @Override
     public void onEnable() {
         // Plugin startup logic
         super.onEnable();
+
+        int pluginId = 21106;
+        metrics metrics = new metrics(this, pluginId);
+
         Objects.requireNonNull(getCommand("jdupe")).setExecutor(new Dupe());
         this.getLogger().info("JayTAK Dupe Plugin Loaded.");
     }
